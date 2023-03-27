29c71bb9ee63e92adcb82099bd74c8f307c2f152
AugustBesh <mr.besh.90@mail.ru>
2023-03-27 14:07:50 +0300

    Добавьте файлы проекта.

---
+++ Pages/PageDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppPractwork1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageDialog.xaml
+    /// </summary>
+    public partial class PageDialog : Page
+    {
+        public PageDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"D:\";
+            if (ofd.ShowDialog() == false)
+                return;
+            string filename = ofd.FileName;
+            string fileText = System.IO.File.ReadAllText(filename);
+            MessageBox.Show($"Файл {filename} открыт. Пароль {fileText}");
+        }
+    }
+}
